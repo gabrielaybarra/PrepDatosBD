348215aa2909ec65e979b2e7f9d9eaaad2831a10
Nerea Zuaznabar <nerea.zuaznabar@alumni.mondragon.edu>
2023-11-04 11:11:14 +0100

    script final de preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -10,14 +10,16 @@ import numpy as np
 
 
 
-class Preprocess():
+class Preprocess:
 
-    def describe_var(df, variables, tipo_var):
+    def __init__(self, df):
+        self.df = df
+
+    def describe_var(self, variables, tipo_var):
 
         """This method will be used to describe one or more columns from a dataframe. 
         The description will be: Count, min, pct 25, mean, median, pct 75, max, std, NaN count and not NaN count
 
-        :param dataframe df: The dataframe from where one or more columns will be described
         :param list variables: Column or list of columns that will be described
         :param str tipo_var: Type of columns that will be described, it manages to options, "cat" or "num"
 
@@ -31,21 +33,21 @@ class Preprocess():
             
             if tipo_var == 'num':
                 for variable in variables:
-                    if df[variable].dtype not in ['int64', 'float64']:
+                    if self.df[variable].dtype not in ['int64', 'float64']:
                         raise TypeError(f"La variable '{variable}' no es numérica.")
                 
                 data = []
                 for variable in variables:
-                    count = df[variable].count()
-                    min_val = df[variable].min()
-                    pct25 = np.nanpercentile(df[variable], 25)
-                    mean = df[variable].mean()
-                    median = df[variable].median()
-                    pct75 = np.nanpercentile(df[variable], 75)
-                    max_val = df[variable].max()
-                    std_dev = df[variable].std()
-                    na_count = df[variable].isna().sum()
-                    notna_count = df[variable].notna().sum()
+                    count = self.df[variable].count()
+                    min_val = self.df[variable].min()
+                    pct25 = np.nanpercentile(self.df[variable], 25)
+                    mean = self.df[variable].mean()
+                    median = self.df[variable].median()
+                    pct75 = np.nanpercentile(self.df[variable], 75)
+                    max_val = self.df[variable].max()
+                    std_dev = self.df[variable].std()
+                    na_count = self.df[variable].isna().sum()
+                    notna_count = self.df[variable].notna().sum()
 
                     data.append([
                         count, min_val, pct25, mean, median, pct75, max_val, std_dev, na_count, notna_count
@@ -56,16 +58,16 @@ class Preprocess():
             
             elif tipo_var == 'cat':
                 for variable in variables:
-                    if df[variable].dtype not in ['object', 'category']:
+                    if self.df[variable].dtype not in ['object', 'category']:
                         raise TypeError(f"La variable '{variable}' no es categórica.")
                 
                 cat_data = []
                 for variable in variables:
-                    count = df[variable].count()
-                    mode_val = df[variable].mode().iloc[0]
-                    mode_freq = df[variable].value_counts().iloc[0]
+                    count = self.df[variable].count()
+                    mode_val = self.df[variable].mode().iloc[0]
+                    mode_freq = self.df[variable].value_counts().iloc[0]
                     mode_percentage = (mode_freq / count) * 100
-                    unique_categories = df[variable].nunique()
+                    unique_categories = self.df[variable].nunique()
 
                     cat_data.append([
                         count, unique_categories, mode_val, mode_freq, mode_percentage
@@ -78,11 +80,9 @@ class Preprocess():
             return str(e)
         
        
-    def view_nan_table(df):
+    def view_nan_table(self):
 
         """This method is used to generate and view a NaN table. It contains the number of missing values and the percentage of them for each column.
-
-        :param dataframe df: The dataframe from where the missing values is extracted
 
         :returns: The NaN table
 
@@ -90,14 +90,14 @@ class Preprocess():
         """
     
         try:
-            if not isinstance(df, pd.DataFrame):
+            if not isinstance(self.df, pd.DataFrame):
                 raise TypeError("Invalid input: 'df' must be a pandas DataFrame.")
 
-            na = df.isna().sum()
-            nona = df.notna().sum()
-            pct = (na / len(df)) * 100
+            na = self.df.isna().sum()
+            nona = self.df.notna().sum()
+            pct = (na / len(self.df)) * 100
             total = list(zip(na, nona, pct))
-            tabla = pd.DataFrame(total, index=df.columns)
+            tabla = pd.DataFrame(total, index=self.df.columns)
             tabla.columns = ['NaN', 'not_NaN', 'pct']
             tabla['pct'] = round(tabla['pct'].astype(float), 2)
 
@@ -111,11 +111,10 @@ class Preprocess():
             print(f"Unkown error: {e}")
     
 
-    def drop_column(df, column_list):
+    def drop_column(self, column_list):
 
         """This method is used to drop one or more columns from a dataframe
 
-        :param dataframe df: The dataframe from where one or more columns is deleted
         :param list column_list: Column or list of columns that is deleted
 
         :returns: dataframe
@@ -124,20 +123,25 @@ class Preprocess():
         """
 
         try:
-            df.drop(column_list, axis=1, inplace=True)
+            self.df.drop(column_list, axis=1, inplace=True)
         except KeyError as e:
             print(f"Error: {e} column not found in the DataFrame. Please check the column name.")
-        return df
+        return self.df
     
 
-    def inplace_missings(df, column, method, n_neighbors = 2):
+    def inplace_missings(self, column, method, n_neighbors = 2):
 
         """This method inplaces missing values of a given table with the method wanted.
 
-            :param dataframe df: The table where the missing values will be filled
             :param str column: Column of the df to inplace missing values
-            :param str method: The method that will be used to inplace the missing values
-            :param int n_neighbors: Only used if the method chosen is KNN to inplace missings
+            :param str method: The method that will be used to inplace the missing values. Possible methods:
+                               - 'mean': Inplaces the missings with the mean of the column
+                               - 'before': Inplaces the missings with the previous value in the column
+                               - 'after': Inplaces the missings with the next value in the column
+                               - 'mode': Inplaces the missings with the most common value in the column
+                               - 'median': Inplaces the missings with the median of the column
+                               - 'KNN': Inplaces the missings using KNN Imputer
+            :param int n_neighbors: Only used if the method chosen is 'KNN' to inplace missings
 
             :returns: dataframe with the missing values replaced
 
@@ -145,32 +149,32 @@ class Preprocess():
 
         """
         try:
-            if df[column].isna().sum() == 0:
+            if self.df[column].isna().sum() == 0:
                 raise Exception("There is no missing data in the selected column")
         
             else:
                 if method == 'mean':
-                    mean = df[column].mean()
-                    df[column].fillna(mean, inplace = True)
+                    mean = self.df[column].mean()
+                    self.df[column].fillna(mean, inplace = True)
 
                 if method == 'before':
-                    df[column].fillna(method='ffill', inplace = True)
+                    self.df[column].fillna(method='ffill', inplace = True)
 
                 if method == 'after':
-                    df[column].fillna(method='bfill', inplace = True)
+                    self.df[column].fillna(method='bfill', inplace = True)
 
                 if method == 'mode':
-                    mode = df[column].mode()[0]
-                    df[column].fillna(mode, inplace = True) 
+                    mode = self.df[column].mode()[0]
+                    self.df[column].fillna(mode, inplace = True) 
 
                 if method == 'median':
-                    median = df[column].median()
-                    df[column].fillna(median, inplace = True)
+                    median = self.df[column].median()
+                    self.df[column].fillna(median, inplace = True)
 
                 if method == 'KNN':
                     knn_imputer = KNNImputer(n_neighbors)
-                    imputed_data = knn_imputer.fit_transform(df)
-                    df = pd.DataFrame(imputed_data, columns=df.columns)
+                    imputed_data = knn_imputer.fit_transform(self.df)
+                    self.df = pd.DataFrame(imputed_data, columns=self.df.columns)
                 
         except KeyError as e:
             print(f"Error: Column not recognized. Please check the data.")
@@ -184,16 +188,14 @@ class Preprocess():
         except Exception as e:
             print(f"Error desconocido: {e}")
 
-        return df
+        return self.df
 
-
-## A PARTIR DE AQUI CLASE HEREDADA
 
 class Read_Preprocess(Preprocess):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, df):
+        super().__init__(df)
 
-    def file_to_dataframe(path):
+    def file_to_dataframe(self, path):
         """This method will be used parse files from several extensions to a pandas dataframe
 
         :param path to file path: Path to the file with a certain extension to be parsed
@@ -207,11 +209,11 @@ class Read_Preprocess(Preprocess):
         if extension == 'json':
             with open(path, 'r', encoding = 'utf-8') as f:
                 data = json.load(f)
-            df = pd.DataFrame(data)
+            self.df = pd.DataFrame(data)
 
 
         elif extension == 'csv':
-            df = pd.read_csv(path)
+            self.df = pd.read_csv(path)
 
         elif extension in ('xlsx', 'xls'):
             try:
@@ -219,9 +221,9 @@ class Read_Preprocess(Preprocess):
                 sheet = workbook.active
                 data = sheet.values
                 columns = next(data)
-                df = pd.DataFrame(data, columns=columns)
+                self.df = pd.DataFrame(data, columns=columns)
             except InvalidFileException:
-                df = pd.read_excel(path, engine='xlrd')
+                self.df = pd.read_excel(path, engine='xlrd')
         
         elif extension == 'xml':
             try:
@@ -232,29 +234,28 @@ class Read_Preprocess(Preprocess):
                     data.append([child.tag] + [subchild.text for subchild in child])
                 columns = data[0]
                 data = data[1:]
-                df = pd.DataFrame(data, columns=columns)
+                self.df = pd.DataFrame(data, columns=columns)
             except ET.ParseError:
                 raise ValueError("El archivo XML no contiene datos en formato tabular")
         
         elif extension == 'h5':
-            df = pd.read_hdf(path)
+            self.df = pd.read_hdf(path)
         
         elif extension == 'txt':
             try:
-                df = pd.read_csv(path, sep='\t')
+                self.df = pd.read_csv(path, sep='\t')
             except pd.errors.ParserError:
                 raise ValueError("El archivo txt no contiene datos en formato tabular")
 
         else:
             raise ValueError("Extensión de archivo no compatible")
         
-        return df
+        return self.df
 
 
-    def outlier_detection(df, column_list = []):
+    def outlier_detection(self, column_list = []):
         """This method will be used to plot and detect outliers from one or more columns
 
-        :param dataframe df: The dataframe from where the outliers will be graphed and detected
         :param list column_list: Column or list of columns from where the outliers will be graphed and detected
 
         :returns: Graph
@@ -265,7 +266,7 @@ class Read_Preprocess(Preprocess):
         if len(column_list) > 0:
             try:
                 plt.figure(figsize = (15,8))
-                sns.boxplot(df[column_list], orient='v')
+                sns.boxplot(self.df[column_list], orient='v')
                 plt.title('Boxplots of numerical columns', size = 20)
                 plt.xlabel('Selected columns', size = 12)
                 plt.ylabel('Value', size = 12)
@@ -279,13 +280,13 @@ class Read_Preprocess(Preprocess):
 
         else:
             plt.figure(figsize = (15,8))
-            sns.boxplot(df, orient='v')
+            sns.boxplot(self.df, orient='v')
             plt.title('Boxplots of numerical columns', size = 20)
             plt.xlabel('Columns', size = 12)
             plt.ylabel('Value', size = 12)
             plt.show()  
 
-    def view_nan_graph(tabla_nan):
+    def view_nan_graph(self, nan_table):
 
         """This method is used to graph the missing values of a dataframe.
 
@@ -297,16 +298,14 @@ class Read_Preprocess(Preprocess):
         """
 
         try:
-            # Verifica si tabla_nan es un DataFrame de pandas
-            if not isinstance(tabla_nan, pd.DataFrame):
-                raise TypeError("Invalid input: 'tabla_nan' must be a pandas DataFrame.")
+            if not isinstance(nan_table, pd.DataFrame):
+                raise TypeError("Invalid input: 'nan_table' must be a pandas DataFrame.")
 
-            # Verifica si hay al menos una columna en el DataFrame
-            if len(tabla_nan.columns) == 0:
-                raise ValueError("Invalid input: 'tabla_nan' must have at least one column.")
+            if len(nan_table.columns) == 0:
+                raise ValueError("Invalid input: 'nan_table' must have at least one column.")
 
             plt.figure(figsize=(15, 8))
-            plt.bar(tabla_nan.index, tabla_nan['pct'])
+            plt.bar(nan_table.index, nan_table['pct'])
             plt.title("Pct NaNs", size=20)
             plt.xticks(rotation=90)
             plt.ylabel("Percentage (%)", size=12)
