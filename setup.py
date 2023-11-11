b8964bd83cfbfd5ef26d24956121863959c71e09
Nerea Zuaznabar <nerea.zuaznabar@alumni.mondragon.edu>
2023-11-11 23:29:07 +0100

    librerias bien

--- setup.py
+++ setup.py
@@ -12,16 +12,12 @@ setup(
   keywords = ['Visualize', 'Preprocess', 'Read'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'pandas',
-          'matplotlib',
-          'sklearn',
-          'seaborn',
-          'json',
-          'csv',
-          'openpyxl',
-          'xlrd',
-          'openpyxl',
-          'xml',
-          'numpy'
+        'matplotlib',
+        'scikit-learn',
+        'seaborn',
+        'openpyxl',
+        'xlrd',
+        'numpy'
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
